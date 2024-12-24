49385ed8aff86b993a0fe31c12f8d5724226b581
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-24 12:32:21 +0000

    add progress indicator

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@ int main() {
         << max_colour_value << "\n";
 
     for (int j = 0; j < height; j++) {
+        std::clog
+            << "\rScanlines remaining: " << (height - j) << " " << std::flush;
         for (int i = 0; i < width; i++) {
             auto r = double(i) / (width - 1);
             auto g = double(j) / (height - 1);
@@ -27,5 +29,5 @@ int main() {
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
-
+    std::clog << "\rDone.                    \n";
 }

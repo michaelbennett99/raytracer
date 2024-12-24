76909000645fdc4c59b71caa16e6424fc0027b29
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-12-24 12:20:48 +0000

    add simple ppm generator

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+    // Image
+    const int width = 256;
+    const int height = 256;
+    const int max_colour_value = 255;
+    const double colour_multiplier = max_colour_value + 0.999;
+
+    // Render
+
+    std::cout
+        << "P3\n"
+        << width << " " << height << "\n"
+        << max_colour_value << "\n";
+
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+            auto r = double(i) / (width - 1);
+            auto g = double(j) / (height - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(colour_multiplier * r);
+            int ig = static_cast<int>(colour_multiplier * g);
+            int ib = static_cast<int>(colour_multiplier * b);
+
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+
+}

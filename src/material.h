efe351afb2fb872129f22158f90e47c4ea17701c
Michael Bennett <michael.d.a.bennett@outlook.com>
2025-01-02 13:34:29 +0000

    Refactor material class constructors and variable initializations for consistency
    Updated constructors in the material classes (`Lambertian`, `Metal`, `Dielectric`, `DiffuseLight`, and `Isotropic`) to use explicit constructors and uniform brace initialization. This change enhances code clarity and maintains a consistent coding style across the codebase.

--- src/material.h
+++ src/material.h
@@ -30,8 +30,13 @@ class Lambertian : public Material {
         std::shared_ptr<Texture> tex;
 
     public:
-        Lambertian(const Colour& a) : tex(std::make_shared<SolidColour>(a)) {}
-        Lambertian(std::shared_ptr<Texture> tex) : tex(tex) {}
+        Lambertian() = delete;
+        explicit Lambertian(const Colour& a)
+            : tex { std::make_shared<SolidColour>(a) }
+        {}
+        explicit Lambertian(std::shared_ptr<Texture> tex)
+            : tex { tex }
+        {}
 
         bool scatter(
             const Ray& r_in,
@@ -39,11 +44,11 @@ class Lambertian : public Material {
             Colour& attenuation,
             Ray& scattered
         ) const override {
-            auto scatter_direction = rec.normal + random_unit_vector();
+            auto scatter_direction { rec.normal + random_unit_vector() };
             if (scatter_direction.near_zero()) {
                 scatter_direction = rec.normal;
             }
-            scattered = Ray(rec.p, scatter_direction, r_in.time());
+            scattered = Ray { rec.p, scatter_direction, r_in.time() };
             attenuation = tex->value(rec.u, rec.v, rec.p);
             return true;
         }
@@ -55,7 +60,8 @@ class Metal : public Material {
         double fuzz;
 
     public:
-        Metal(const Colour& a, double f) : albedo(a), fuzz(f) {}
+        Metal() = delete;
+        explicit Metal(const Colour& a, double f) : albedo { a }, fuzz { f } {}
 
         bool scatter(
             const Ray& r_in,
@@ -63,15 +69,13 @@ class Metal : public Material {
             Colour& attenuation,
             Ray& scattered
         ) const override {
-            const auto reflected = reflect(
-                r_in.direction(),
-                rec.normal
-            );
-            const auto fuzzed = unit_vector(reflected)
-                + (fuzz * random_unit_vector());
-            scattered = Ray(rec.p, fuzzed, r_in.time());
+            const auto reflected { reflect(r_in.direction(), rec.normal) };
+            const auto fuzzed {
+                unit_vector(reflected) + (fuzz * random_unit_vector())
+            };
+            scattered = Ray { rec.p, fuzzed, r_in.time() };
             attenuation = albedo;
-            return (dot(scattered.direction(), rec.normal) > 0);
+            return dot(scattered.direction(), rec.normal) > 0;
         }
 };
 
@@ -81,13 +85,16 @@ class Dielectric : public Material {
 
         static double reflectance(double cosine, double ref_idx) {
             // Schlick's approximation
-            auto r0 = (1 - ref_idx) / (1 + ref_idx);
-            r0 = r0 * r0;
-            return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+            const auto r0 { (1 - ref_idx) / (1 + ref_idx) };
+            const auto r0_squared { r0 * r0 };
+            return r0_squared + (1 - r0_squared) * std::pow((1 - cosine), 5);
         }
 
     public:
-        Dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+        Dielectric() = delete;
+        explicit Dielectric(double index_of_refraction)
+            : ir { index_of_refraction }
+        {}
 
         bool scatter(
             const Ray& r_in,
@@ -96,23 +103,28 @@ class Dielectric : public Material {
             Ray& scattered
         ) const override {
             attenuation = Colour(1.0, 1.0, 1.0);
-            const auto refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
-            const auto unit_direction = unit_vector(r_in.direction());
+            const auto refraction_ratio {
+                rec.front_face ? (1.0 / ir) : ir
+            };
+            const auto unit_direction { unit_vector(r_in.direction()) };
 
-            const auto cos_theta = std::fmin(
-                dot(-unit_direction, rec.normal),
-                1.0
-            );
-            const auto sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+            const auto cos_theta {
+                std::fmin(dot(-unit_direction, rec.normal), 1.0)
+            };
+            const auto sin_theta { std::sqrt(1.0 - cos_theta * cos_theta) };
 
-            const auto cannot_refract = refraction_ratio * sin_theta > 1.0;
-            const auto wont_refract = reflectance(cos_theta, refraction_ratio)
-                    > gen_rand::random_double();
-            const auto direction = cannot_refract || wont_refract
-                ? reflect(unit_direction, rec.normal)
-                : refract(unit_direction, rec.normal, refraction_ratio);
+            const auto cannot_refract { refraction_ratio * sin_theta > 1.0 };
+            const auto wont_refract {
+                reflectance(cos_theta, refraction_ratio)
+                    > gen_rand::random_double()
+            };
+            const auto direction {
+                cannot_refract || wont_refract
+                    ? reflect(unit_direction, rec.normal)
+                    : refract(unit_direction, rec.normal, refraction_ratio)
+            };
 
-            scattered = Ray(rec.p, direction, r_in.time());
+            scattered = Ray { rec.p, direction, r_in.time() };
             return true;
         }
 };
@@ -122,9 +134,11 @@ class DiffuseLight : public Material {
         std::shared_ptr<Texture> tex;
 
     public:
-        DiffuseLight(std::shared_ptr<Texture> tex) : tex(tex) {}
-        DiffuseLight(const Colour& emit)
-            : tex(std::make_shared<SolidColour>(emit)) {}
+        DiffuseLight() = delete;
+        explicit DiffuseLight(std::shared_ptr<Texture> tex) : tex { tex } {}
+        explicit DiffuseLight(const Colour& emit)
+            : tex { std::make_shared<SolidColour>(emit) }
+        {}
 
         Colour emitted(double u, double v, const Point3& p) const override {
             return tex->value(u, v, p);
@@ -136,9 +150,11 @@ class Isotropic : public Material {
         std::shared_ptr<Texture> tex;
 
     public:
-        Isotropic(const Colour& albedo)
-            : tex(std::make_shared<SolidColour>(albedo)) {}
-        Isotropic(std::shared_ptr<Texture> tex) : tex(tex) {}
+        Isotropic() = delete;
+        explicit Isotropic(const Colour& albedo)
+            : tex { std::make_shared<SolidColour>(albedo) }
+        {}
+        explicit Isotropic(std::shared_ptr<Texture> tex) : tex { tex } {}
 
         bool scatter(
             const Ray& r_in,
@@ -146,7 +162,7 @@ class Isotropic : public Material {
             Colour& attenuation,
             Ray& scattered
         ) const override {
-            scattered = Ray(rec.p, random_unit_vector(), r_in.time());
+            scattered = Ray { rec.p, random_unit_vector(), r_in.time() };
             attenuation = tex->value(rec.u, rec.v, rec.p);
             return true;
         }
